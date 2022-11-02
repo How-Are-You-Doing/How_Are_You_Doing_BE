174f5f7b9f514d3fd2400c1a6b4ec5d289dd3629
Carter Ball <carter.k.ball@gmail.com>
2022-11-02 19:20:02 -0400

    Create friend factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :post do
     description { Faker::Lorem.sentence }
-    post_status { rand(0..2) }
+    post_status { rand(0..1) }
     tone { Faker::Emotion.noun }
     user
     emotion
