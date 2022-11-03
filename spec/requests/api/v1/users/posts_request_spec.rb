49c11a3aeaaed20f89e9af626abb96a61e5ba7b1
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-03 12:20:48 -0600

    Test: Add testing for user posts index

---
+++ spec/requests/api/v1/users/posts_request_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe 'Posts API' do
+  describe 'user post index' do
+    describe 'happy path' do
+      it 'sends a list of all posts for a user' do
+        create_list(:post, 5)
+
+        get '/api/v1/users/history'
+
+        expect(response).to be_successful
+
+        posts = JSON.parse(response.body, symbolize_names: true)
+
+        expect(posts[:data].count).to eq(5)
+
+        posts[:data].each do |post|
+          expect(post).to have_key(:id)
+          expect(post[:id]).to be_a(String)
+
+          expect(post).to have_key(:type)
+          expect(post[:type]).to be_a(String)
+
+          expect(post).to have_key(:attributes)
+          expect(post[:attributes]).to be_a(Hash)
+
+          expect(post[:attributes]).to have_key(:emotion)
+          expect(post[:attributes][:emotion]).to be_a(String)
+
+          expect(post[:attributes]).to have_key(:post_status)
+          expect(post[:attributes][:post_status]).to be_a(String)
+
+          expect(post[:attributes]).to have_key(:description)
+          expect(post[:attributes][:description]).to be_a(String)
+
+          expect(post[:attributes]).to have_key(:tone)
+          expect(post[:attributes][:tone]).to be_a(String)
+
+          expect(post[:attributes]).to have_key(:created_at)
+          expect(post[:attributes][:created_at]).to be_a(String)
+        end
+      end
+    end
+
+    describe 'sad path' do
+
+    end
+  end
+end
