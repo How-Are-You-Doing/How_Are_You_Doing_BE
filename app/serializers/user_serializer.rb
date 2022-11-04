d9284bb328beef7e4fd1d66d87d8de2b4d3af9ea
Carter Ball <carter.k.ball@gmail.com>
2022-11-03 20:20:51 -0400

    User search by email endpoint working

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,15 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :email
 
+  def self.user_email(user)
+    {
+      "data": {
+        "id": user.id.to_s,
+        "type": user.class.name.downcase,
+        attributes: {
+            name: user.name
+          }
+      }
+    }
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,11 @@
 require 'faker'
 
-oauth_hash = Faker::Omniauth.google
+
 
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    google_id { oauth_hash[:uid] }
+    google_id { rand(100000.1000000000)}
   end
 end 
