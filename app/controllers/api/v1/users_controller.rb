1e304563647b39fc982b7c609c60624f3eb4b562
Mary Ballantyne <mballantyne777@gmail.com>
2022-11-04 14:35:34 -0600

    adjustments to create user so a new user created on front end is passed to backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::UsersController < ApplicationController 
 
   def create
-    user = User.new(name: request.headers.env["HTTP_NAME"], email: request.headers.env["HTTP_EMAIL"], google_id: request.headers.env["google_id"] )
+    user = User.new(
+      name: request.headers.env["HTTP_NAME"],
+      email: request.headers.env["HTTP_EMAIL"],
+      google_id: request.headers.env["HTTP_GOOGLE_ID"],
+    )
     if user.save
       render json: { message: 'User successfully created' }, status: 201
     end
