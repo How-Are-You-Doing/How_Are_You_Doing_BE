336bdca0e8f11da297a2cace1c0122f960843751
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-04 20:53:37 -0600

    refactor complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,19 +13,31 @@ class Api::V1::UsersController < ApplicationController
 
   def search
     if params[:email]
+      render_user_by_email
+    elsif params[:search]
+      render_user_by_google_id
+    end
+  end
+
+  private
+
+  def render_user_by_email
       if User.exists?(email: params[:email])
         user = User.find_by(email: params[:email])
         render json: UserSerializer.user_email(user)
       else
         render json: {:data=>[]}
       end
-    elsif params[:search]
-      if User.exists?(google_id: params[:search])
-        user = User.find_by(google_id: params[:search])
-        render json: UserSerializer.user_google_id(user)
-      else
-        render json: {:data=>[]}
-      end
+  end
+
+  def render_user_by_google_id
+    if User.exists?(google_id: params[:search])
+      user = User.find_by(google_id: params[:search])
+      render json: UserSerializer.user_google_id(user)
+    else
+      render json: {:data=>[]}
     end
   end
+
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,22 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :email, :google_id
 
+  def self.render_users_friends(users)
+    users.map do |user|
+      {
+        "data": {
+          "id": user.id.to_s,
+          "type": user.class.name.downcase,
+          attributes: {
+              name: user.name,
+              name: user.email
+            }
+        }
+      }
+    end
+  end
+
+
   def self.user_email(user)
     {
       "data": {
@@ -13,7 +29,6 @@ class UserSerializer
       }
     }
   end
-
 
   def self.user_google_id(user)
     {
