e2c77ea1bea03d934e7a13f0f2843e32fd07c4d0
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-04 18:50:25 -0600

    Refactor: formating updates

--- spec/requests/api/v2/users/posts_request_spec.rb
+++ spec/requests/api/v2/users/posts_request_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe 'Posts API' do
 
   describe 'user post index' do
-    
+
     describe 'happy path' do
+
       it 'sends a list of all posts for a user' do
         user = create(:user)
         posts = create_list(:post, 5, user: user)
 
-        params = {user: "#{user.google_id}"}
+        params = { user: "#{user.google_id}" }
 
         get '/api/v2/users/history', params: params
 
@@ -48,6 +49,7 @@ describe 'Posts API' do
     end
 
     describe 'sad path' do
+      
       it 'sends a 400 error if no user google_id recived in params for a user' do
         user = create(:user)
         posts = create_list(:post, 5, user: user)
@@ -57,7 +59,7 @@ describe 'Posts API' do
         posts = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to have_http_status(400)
-        
+
         expect(posts).to have_key(:data)
         expect(posts[:data]).to be_a(Hash)
       end
@@ -65,14 +67,13 @@ describe 'Posts API' do
       it 'sends a empty array is user has no posts' do
         user = create(:user)
 
-        params = {user: "#{user.google_id}"}
+        params = { user: "#{user.google_id}" }
 
         get '/api/v2/users/history', params: params
 
         posts = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to be_successful
-
 
         expect(posts).to have_key(:data)
         expect(posts[:data]).to be_an(Array)
@@ -81,14 +82,13 @@ describe 'Posts API' do
       it 'sends a empty data hash if google_id for user is not found' do
         user = create(:user)
 
-        params = {user:> "65"}
+        params = { user: '65' }
 
         get '/api/v2/users/history', params: params
 
         posts = JSON.parse(response.body, symbolize_names: true)
 
         expect(response).to have_http_status(400)
-
 
         expect(posts).to have_key(:data)
         expect(posts[:data]).to be_an(Hash)
@@ -97,15 +97,17 @@ describe 'Posts API' do
   end
 
   describe 'user post most_recent' do
+
     describe 'happy path' do
+
       it 'returns a users most recent post if a user has posts' do
         user = create(:user)
         oldest_post = create(:post, user: user, created_at: 100.day.ago)
         middle_post = create(:post, user: user, created_at: 50.day.ago)
         newest_post = create(:post, user: user, created_at: 1.day.ago)
         create_list(:post, 5)
-       
-        params = {user: "#{user.google_id}"}
+
+        params = { user: "#{user.google_id}" }
 
         get '/api/v2/posts/last', params: params
 
@@ -113,12 +115,12 @@ describe 'Posts API' do
 
         post_data = JSON.parse(response.body, symbolize_names: true)
         post = post_data[:data]
-      
+
         expect(post_data).to be_a(Hash)
         expect(post_data.count).to eq(1)
 
         expect(post[:id].to_i).to eq(newest_post.id)
-        expect(post[:type]).to eq("post")
+        expect(post[:type]).to eq('post')
         expect(post[:attributes].count).to eq(5)
         expect(post[:attributes][:emotion]).to eq(newest_post.emotion.term)
         expect(post[:attributes][:post_status]).to eq(newest_post.post_status)
@@ -129,20 +131,20 @@ describe 'Posts API' do
     end
 
     describe 'sad path' do
+
       it 'returns an empty array if a user has no posts' do
         user = create(:user)
         create_list(:post, 5)
 
-        params = {user: "#{user.google_id}"}
+        params = { user: "#{user.google_id}" }
 
         get '/api/v2/posts/last', params: params
 
         expect(response).to be_successful
 
         post_data = JSON.parse(response.body, symbolize_names: true)
-      
-        expect(post_data).to eq({:data=>{}})
 
+        expect(post_data).to eq({ data: {} })
       end
     end
   end
