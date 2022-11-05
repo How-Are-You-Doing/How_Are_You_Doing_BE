1082f0580f8087f01a905f7b9602b2d20e5c2a05
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-04 19:44:25 -0600

    tests created

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -63,7 +63,43 @@ describe "Users API" do
         user_data = JSON.parse(response.body, symbolize_names: true)
         expect(user_data).to eq({:data=>[]})
       end
+    end
 
+    describe 'happy path' do
+      it 'can find a user given a google id as a param' do
+        u1 = create(:user)
+      
+        # headers = ({email: u1.google_id})
+        
+        get "/api/v1/users?search=#{u1.google_id}" #was taken from the seed file google_id list
+      
+        expect(response).to be_successful
+
+        user_data = JSON.parse(response.body, symbolize_names: true)
+        user = user_data[:data]
+require 'pry' ; binding.pry
+        expect(user[:id].to_i).to eq(u1.id)
+        expect(user[:type]).to eq("user")
+        expect(user[:attributes].count).to eq(3)
+        expect(user[:attributes][:name]).to eq(u1.name)
+        expect(user[:attributes][:email]).to eq(u1.email)
+        expect(user[:attributes][:google_id]).to eq(u1.google_id)
+      end
+    end
+
+    describe 'sad path' do
+      it 'returns an empty array if there is no user by that google id' do
+        u1 = create(:user)
+      
+        # headers = ({email: u1.email})
+        
+        get "/api/v1/users?search=000000000000"
+
+        expect(response).to be_successful
+
+        user_data = JSON.parse(response.body, symbolize_names: true)
+        expect(user_data).to eq({:data=>[]})
+      end
     end
   end
 end
