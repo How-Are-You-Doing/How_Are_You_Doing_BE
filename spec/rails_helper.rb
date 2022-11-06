4790df45e7012a249186b12a18830115354210ce
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-11-05 19:21:58 -0600

    added filter sensitive data to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,5 +82,6 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data("<tone_api_key>") { ENV['tone_api_key']}
   config.default_cassette_options = { re_record_interval: 5.days }
 end
