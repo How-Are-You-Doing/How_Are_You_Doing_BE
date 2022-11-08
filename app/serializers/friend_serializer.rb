3ccd0cc2cf66adda620ad13e81c36ceaa807b243
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-07 20:20:49 -0700

    Feat: Add options for serializing friendship_id and a users friends/followers info in same end point.

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -1,4 +1,10 @@
 class FriendSerializer
   include JSONAPI::Serializer
   attributes :follower_id, :followee_id, :request_status
+  attributes :follower do  |friend|
+    UserSerializer.new(User.find(friend.follower_id))
+  end
+  attributes :followee do  |friend|
+    UserSerializer.new(User.find(friend.followee_id))
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,42 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :name, :email, :google_id
+
+  def self.followees(followee_friends)
+    {
+      data:
+      followee_friends.map do |friend|
+        user = User.find(friend.followee_id)
+        {
+          id: user.id,
+          friendship_id: friend.id,
+          type: "friend_followee",
+          attributes: {
+            name: user.name,
+            email: user.email,
+            google_id: user.google_id
+          }
+        }
+      end
+    }
+  end
+
+  def self.followers(follower_friends)
+    {
+      data: 
+      follower_friends.map do |friend|
+        user = User.find(friend.follower_id)
+        {
+          id: user.id,
+          friendship_id: friend.id,
+          type: "friend_follower",
+          attributes: {
+            name: user.name,
+            email: user.email,
+            google_id: user.google_id
+          }
+        }
+      end
+    }
+  end
 end
