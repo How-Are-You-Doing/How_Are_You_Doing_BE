e32971ab3f3aba84d124adf71d690e9d506bc85d
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-07 20:55:54 -0700

    Commented out unused code in Friends serilizer

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -1,10 +1,11 @@
-class FriendSerializer
-  include JSONAPI::Serializer
-  attributes :follower_id, :followee_id, :request_status
-  attributes :follower do  |friend|
-    UserSerializer.new(User.find(friend.follower_id))
-  end
-  attributes :followee do  |friend|
-    UserSerializer.new(User.find(friend.followee_id))
-  end
-end
+# NOT used can DELETE prior to finalizing
+# class FriendSerializer
+#   include JSONAPI::Serializer
+#   attributes :follower_id, :followee_id, :request_status
+#   attributes :follower do  |friend|
+#     UserSerializer.new(User.find(friend.follower_id))
+#   end
+#   attributes :followee do  |friend|
+#     UserSerializer.new(User.find(friend.followee_id))
+#   end
+# end
