9948918dd4a5087f4744c35970d46ff0047c618e
Carter Ball <carter.k.ball@gmail.com>
2022-11-09 11:21:52 -0500

    Updates to check_for_existing_request validation to pass model tests

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,12 +1,9 @@
 class Friend < ApplicationRecord
-  # include Comparable
-  
   enum request_status: { pending: 0, accepted: 1, rejected: 2 }
 
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
 
-  # validates :followee_id, comparison: { other_than: :follower_id }
   validates_presence_of :follower_id, :followee_id, :request_status
   validates_numericality_of :follower_id, :followee_id
   validate :check_for_requesting_self
@@ -24,6 +21,7 @@ class Friend < ApplicationRecord
   end
 
   def check_for_existing_request
+    return true if follower_id.nil? || followee_id.nil?
     follower = User.find_by(id: follower_id)
     followee = User.find_by(id: followee_id)
     follower_followed_ids = follower.all_followees_ids
